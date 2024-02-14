16d75e07f1a5a31a3952184cccc9ef21388ed9a6
jatin2507 <jujatin@gmail.com>
2024-02-14 23:37:46 +0530

    zrok

---
+++ server.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const app = express();
+
+// Your express routes here
+app.get('/', (req, res) => { res.send('Hello World!') });
+
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+
+const httpServer = http.createServer(app);
+
+
+httpServer.listen(80, () => console.log('HTTP Server running on port 80'));
