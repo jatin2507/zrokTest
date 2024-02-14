43ba4638db5c3fd88c5df7f276a1748e6549d739
jatin2507 <jujatin@gmail.com>
2024-02-14 23:46:46 +0530

    script

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express();
 
 // Your express routes here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
