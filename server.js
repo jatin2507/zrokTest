8bf3d220b8a372953002eabe0d20fab28237bb71
jatin2507 <jujatin@gmail.com>
2024-02-14 23:41:30 +0530

    script

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ const credentials = {
 const httpServer = http.createServer(app);
 
 
-httpServer.listen(80, () => console.log('HTTP Server running on port 80'));
+httpServer.listen(3003, () => console.log('HTTP Server running on port 80'));
